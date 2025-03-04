bfcc0f10157d3958e0c432f04006d1bfc8bd89a2
blankMCU <vexown@gmail.com>
2025-03-04 14:09:35 +0100

    Include CTest and update CTestConfig.cmake with CDash dashboard details

--- Official_CMakeTutorial/Step6/CTestConfig.cmake
+++ Official_CMakeTutorial/Step6/CTestConfig.cmake
@@ -1,3 +1,9 @@
+# For this tutorial, a public dashboard server is used and its corresponding CTestConfig.cmake file is provided for you in this step's root directory. 
+# In practice, this file would be downloaded from a project's Settings page on the CDash instance intended to host the test results. 
+# Once downloaded from CDash, the file should not be modified locally.
+
+# See more details here: https://cmake.org/cmake/help/latest/guide/tutorial/Adding%20Support%20for%20a%20Testing%20Dashboard.html
+
 set(CTEST_PROJECT_NAME "CMakeTutorial")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
